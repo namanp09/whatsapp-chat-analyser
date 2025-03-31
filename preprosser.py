78d128646db35418b86a15495eaacf0461838804
NAMAN <mac@Namans-MacBook-Air.local>
2025-03-31 16:19:36 +0530

    Initial commit

---
+++ preprosser.py
@@ -0,0 +1,62 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = r"\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}"
+
+    messages = re.split(pattern, data)[1:]  # Skip the first empty value
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    # Convert to datetime format
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %H:%M', errors='coerce')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+
+    users = []
+    messages = []
+
+    # Ensure column name has no extra spaces
+    df.columns = df.columns.str.strip()
+
+    # Loop through each message
+    for message in df['user_message']:  # Ensure correct column name
+        entry = re.split(r'([\w\W]+?):\s', message, maxsplit=1)  # Use raw string
+
+        if len(entry) > 1:
+            users.append(entry[1])  # User name
+            messages.append(entry[2])  # Actual message
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    # Add parsed data to DataFrame
+    df['user'] = users
+    df['message'] = messages
+
+    # Remove the old column
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date']=df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour']=df['date'].dt.hour
+    df['minute']=df['date'].dt.minute
+
+    period=[]
+    for hour in df[['day_name','hour']]['hour']:
+        if hour==23:
+            period.append(str(hour) + "-" + str('00'))
+        elif hour==0:
+            period.append(str('00') + "-" + str(hour+1))
+        else:
+            period.append(str(hour) + "-" + str(hour+1))
+
+    df['period'] = period
+
+    return df
